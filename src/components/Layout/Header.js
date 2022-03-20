c17f07048e00d744206ef6873fd325cc12bdceba
jaltaie <jaltaie@gmail.com>
2022-03-20 20:27:50 +0300

    First commit for React test material

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import {Fragment} from 'react';
+import mealsImage from '../../assets/meals.jpeg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = props => {
+    return <Fragment>
+        <header className={
+            classes.header
+        }>
+            <h1>React meals</h1>
+            <HeaderCartButton onClick={props.onShowCart}/>
+        </header>
+        <div className={
+            classes['main-image']
+        }>
+            <img src={mealsImage}
+                alt="a table full of food"/>
+        </div>
+    </Fragment>
+};
+
+export default Header;
